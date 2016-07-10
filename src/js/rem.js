eb95cbea54260ecd944d9ea15ab5c06dd936eb55
Perlou <a87387875@163.com>
2016-07-10 11:40:40 +0800

    init

---
+++ src/js/rem.js
@@ -0,0 +1,10 @@
+/**
+ * @author Perlou
+ * @rem.js
+ */
+
+'use strict';
+
+(function(){
+	document.getElementsByTagName('html')[0].style.fontSize = document.documentElement.clientWidth/16 + 'px' || window.innerWidth/16 + 'px';
+})();
